e1eae7d29e31d6c645652dde337af227f0034d79
Jackson <jacksonlimag@hotmail.com>
2024-01-24 18:09:44 -0300

    create items controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  get 'items/new'
+  get 'items/edit'
+  get 'items/create'
+  get 'items/update'
+  get 'items/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
